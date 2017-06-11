d29547adbd8d8a78a25378e33dc2e2b983bba9ca
chenyuan <ychen@i-con.cn>
2017-06-11 20:52:23 +0800

    store

--- ch02.cpp
+++ ch02.cpp
@@ -14,7 +14,7 @@ class Widget<FooWindow, FooController>
 {
 public:
     void print() {std::cout << "Foo print" << std::endl;}
-}
+};
 
 class BarWindow;
 template <typename Controller>
@@ -22,9 +22,20 @@ class Widget<BarWindow, Controller>
 {
 public:
     void print() {std::cout << "Bar print" << std::endl;}
-} 
+};
 
 int main(int argc, char **argv)
 {
+    Widget<int, int> intWidget;
+    intWidget.print();
+
+    Widget<FooWindow, int> fooIntWidget;
+    fooIntWidget.print();
+
+    Widget<FooWindow, FooController> fooWidget;
+    fooWidget.print();
+
+    Widget<BarWindow, int> barWidget;
+    barWidget.print();
     return 0;
 }
