5a34a042d55fa28fc198d9cae39f47933064c60c
chenchunhh <chenchunhh@lcoudy.lcoudy>
2017-06-12 00:00:57 +0800

    store

--- SConstruct
+++ SConstruct
@@ -1,2 +1,2 @@
 Program("test", ["test.cpp"])
-Program("ch02", ["ch02.cpp"], CXXFLAGS="-O2 -std=c++11")
+Program("ch02", ["ch02.cpp"], CXXFLAGS="-O2 -std=c++11", CXX="clang++")
--- ch02.cpp
+++ ch02.cpp
@@ -1,51 +1,6 @@
 #include <iostream>
 #include <string>
 
-template <typename Window, typename Controller>
-class Widget {
-public:
-    void print() {std::cout << "General Print" << std::endl;}
-};
-
-class FooWindow;
-class FooController;
-
-template <>
-class Widget<FooWindow, FooController>
-{
-public:
-    void print() {std::cout << "Foo print" << std::endl;}
-};
-
-class BarWindow;
-template <typename Controller>
-class Widget<BarWindow, Controller>
-{
-public:
-    void print() {std::cout << "Bar print" << std::endl;}
-};
-
-template <typename ButtonArg>
-class Button;
-
-template <typename ButtonArg, typename Controller>
-class Widget<Button<ButtonArg>, Controller>
-{
-public:
-    void print() {std::cout << "Button print" << std::endl;}
-};
-
-void Func()
-{
-    class Local {
-    public:
-        void print() {std::cout << __func__ << std::endl;};
-    };
-
-    Local local;
-    local.print();
-}
-
 template <int v>
 class Int2Type {
 public:
@@ -65,7 +20,7 @@ private:
 
 class Widget {
 public:
-    template <typename T> Widget() {}
+    template <typename T> Widget(const T&) {}
 };
 
 template <typename T, typename U>
