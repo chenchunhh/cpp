1db0beb2b494f5059b75ef6de2a3071f471c362c
chenyuan <ychen@i-con.cn>
2017-06-11 20:50:01 +0800

    store

--- ch02.cpp
+++ ch02.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
 
+template <typename Window, typename Controller>
+class Widget {
+public:
+    void print() {std::cout << "General Print" << std::endl;}
+};
+
+class FooWindow;
+class FooController;
+
+template <>
+class Widget<FooWindow, FooController>
+{
+public:
+    void print() {std::cout << "Foo print" << std::endl;}
+}
+
+class BarWindow;
+template <typename Controller>
+class Widget<BarWindow, Controller>
+{
+public:
+    void print() {std::cout << "Bar print" << std::endl;}
+} 
+
 int main(int argc, char **argv)
 {
     return 0;
